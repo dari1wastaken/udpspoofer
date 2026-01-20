f5464c6d294f47067cedb1627700d81a9a24d833
dari1wastaken <darioferrix@gmail.com>
2026-01-20 14:53:25 +0000

    fix: update spoofer build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,9 @@
-docker build --rm -t ackspoofer_database:latest .
+docker build --rm -t udpspoofer_database:latest .
 docker image prune -f
 echo "----- START RUNNING CONTAINER -----"
-docker run -it --name ackspoofer_database ackspoofer_database
+docker run -it --name udpspoofer_database udpspoofer_database
 echo "----- STOP RUNNING CONTAINER -----"
 mkdir bin
-docker cp ackspoofer_database:/app/godocker bin/ackspoofer_database
-docker stop ackspoofer_database
-docker rm ackspoofer_database
+docker cp udpspoofer_database:/app/godocker bin/udpspoofer_database
+docker stop udpspoofer_database
+docker rm udpspoofer_database
