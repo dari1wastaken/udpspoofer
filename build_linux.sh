3ef0b9ae074fb03a6dd14d814e859160fc6aafb1
dari1wastaken <darioferrix@gmail.com>
2026-02-13 15:20:28 +0000

    fix script

--- build_linux.sh
+++ build_linux.sh
@@ -3,7 +3,7 @@ docker image prune -f
 echo "----- START RUNNING CONTAINER -----"
 docker run -it --name udpspoofer_database udpspoofer_database
 echo "----- STOP RUNNING CONTAINER -----"
-mkdir bin
+mkdir -p bin
 docker cp udpspoofer_database:/app/godocker bin/udpspoofer_database
 docker stop udpspoofer_database
 docker rm udpspoofer_database
