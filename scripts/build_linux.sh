b463071b101dd16822befa5a47f8fe8896337064
dari1wastaken <darioferrix@gmail.com>
2026-03-10 20:43:12 +0000

    fix(docker): binary build

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,9 +1,10 @@
-docker build --rm --no-cache -t udpspoofer_database:latest .
+# Run from the project root
+docker build -f ./docker/Dockerfile --rm --no-cache -t udpspoofer_database:latest .
 docker image prune -f
 echo "----- START RUNNING CONTAINER -----"
 docker run -it --name udpspoofer_database udpspoofer_database
 echo "----- STOP RUNNING CONTAINER -----"
 mkdir -p bin
-docker cp udpspoofer_database:/app/godocker bin/udpspoofer_database
+docker cp udpspoofer_database:/app/godocker ./bin/udpspoofer_database
 docker stop udpspoofer_database
 docker rm udpspoofer_database
